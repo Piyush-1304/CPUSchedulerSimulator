7b2185e69a450e7d93b1b0a607dafe9cb2e85624
{piyush} <{13piyush04@gmail.com}>
2025-03-26 11:23:53 +0530

    Updating fcfs & priorityScheduling

--- fcfs.py
+++ fcfs.py
@@ -3,43 +3,15 @@ class FCFS:
         self.processes = sorted(processes, key=lambda x: x[1])  # Sort by arrival time
 
     def calculate_completion_time(self):
-        completion_time = 0
-        schedule = []
+        current_time = 0
+        timeline = []
         
         for pid, arrival, burst in self.processes:
-            if completion_time < arrival:
-                completion_time = arrival  # Wait if CPU is idle
-            completion_time += burst
-            schedule.append((pid, completion_time))
+            if current_time < arrival:
+                current_time = arrival  # Wait if CPU is idle
+            start_time = current_time
+            end_time = start_time + burst
+            timeline.append((pid, start_time, end_time))
+            current_time = end_time
         
-        return schedule
-class FCFS:
-    def __init__(self, processes):
-        self.processes = sorted(processes, key=lambda x: x[1])  # Sort by arrival time
-
-    def calculate_completion_time(self):
-        completion_time = 0
-        schedule = []
-        
-        for pid, arrival, burst in self.processes:
-            if completion_time < arrival:
-                completion_time = arrival  # Wait if CPU is idle
-            completion_time += burst
-            schedule.append((pid, completion_time))
-        
-        return schedule
-class FCFS:
-    def __init__(self, processes):
-        self.processes = sorted(processes, key=lambda x: x[1])  # Sort by arrival time
-
-    def calculate_completion_time(self):
-        completion_time = 0
-        schedule = []
-        
-        for pid, arrival, burst in self.processes:
-            if completion_time < arrival:
-                completion_time = arrival  # Wait if CPU is idle
-            completion_time += burst
-            schedule.append((pid, completion_time))
-        
-        return schedule
+        return timeline
